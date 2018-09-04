5f81b048872ab79c85adca261013bb849347c577
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-09-04 10:55:17 -0400

    Update ha.vhdl

--- ha.vhdl
+++ ha.vhdl
@@ -1,18 +1,18 @@
-library ieee;
+library ieee;               -- Nerdy Dave https://youtu.be/H2GyAIYwZbw
 use ieee.std_logic_1164.all;
 
-entity ha is
+entity ha is                -- Half Adder
     port
     (
         a:  in  std_ulogic;
         b:  in  std_ulogic;
-        o:  out std_ulogic;
-        c:  out std_ulogic
+        s:  out std_ulogic; -- Sum
+        c:  out std_ulogic  -- Carry
     );
 end ha;
 
 architecture behave of ha is
 begin
-    o <= a xor b;
+    s <= a xor b;
     c <= a and b;
 end behave;
