2f1716d65ba93390ce7f1352e48f2f9eb92540bd
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2020-05-27 09:13:20 -0400

    Update adder.vhdl

--- ghdl/adder.vhdl
+++ ghdl/adder.vhdl
@@ -1,4 +1,4 @@
--- https://ghdl.readthedocs.io/en/stable/using/QuickStartGuide.html
+--  https://ghdl.readthedocs.io/en/stable/using/QuickStartGuide.html
 entity adder is
   -- `i0`, `i1`, and the carry-in `ci` are inputs of the adder.
   -- `s` is the sum output, `co` is the carry-out.
