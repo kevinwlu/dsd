1a4cded92467041db96acfceeabc8dda6a0bc215
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-26 17:28:02 -0400

    Update hexcount_1.vhd

--- Nexys-A7/Lab-2/Modifications/hexcount_1.vhd
+++ Nexys-A7/Lab-2/Modifications/hexcount_1.vhd
@@ -16,22 +16,22 @@ ARCHITECTURE Behavioral OF hexcount IS
 	COMPONENT counter IS
 		PORT (
 			clk : IN STD_LOGIC;
-			count : OUT STD_LOGIC_VECTOR (15 DOWNTO 0); --NEED CHANGE! counter now output 16 bits for all 4 displays
-			mpx : OUT STD_LOGIC_VECTOR (1 DOWNTO 0)
+			count : OUT STD_LOGIC_VECTOR (31 DOWNTO 0); --NEED CHANGE! counter now output 32 bits for all 8 displays
+			mpx : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
 		);
 	END COMPONENT;
 
 	COMPONENT leddec IS
 		PORT (
-			dig : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+			dig : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
 			data : IN STD_LOGIC_VECTOR (3 DOWNTO 0); --DONT change, data is fixed 4 bits in leddec for each displays
 			anode : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
 			seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
 		);
 	END COMPONENT;
 
-	SIGNAL S : STD_LOGIC_VECTOR (15 DOWNTO 0); -- connect C1 and L1 for values of 4 digits
-	SIGNAL md : STD_LOGIC_VECTOR (1 DOWNTO 0); -- mpx selects displays
+	SIGNAL S : STD_LOGIC_VECTOR (31 DOWNTO 0); -- connect C1 and L1 for values of 8 digits
+	SIGNAL md : STD_LOGIC_VECTOR (2 DOWNTO 0); -- mpx selects displays
 	SIGNAL display : STD_LOGIC_VECTOR (3 DOWNTO 0); -- send digit for only one display to leddec
 
 BEGIN
