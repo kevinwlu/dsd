f1b0fc4bb4f581d4db0bee6e9916ce029aab1887
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-27 08:55:43 -0400

    Update hexcount.vhd

--- Nexys-A7/Lab-1/hexcount.vhd
+++ Nexys-A7/Lab-1/hexcount.vhd
@@ -4,7 +4,7 @@ USE IEEE.STD_LOGIC_1164.ALL;
 ENTITY hexcount IS
 	PORT (
 		clk_50MHz : IN STD_LOGIC;
-		anode : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+		anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
 		seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
 	);
 END hexcount;
@@ -18,9 +18,9 @@ ARCHITECTURE Behavioral OF hexcount IS
 	END COMPONENT;
 	COMPONENT leddec IS
 		PORT (
-			dig : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+			dig : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
 			data : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-			anode : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+			anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
 			seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
 		);
 	END COMPONENT;
@@ -29,5 +29,5 @@ BEGIN
 	C1 : counter
 	PORT MAP(clk => clk_50MHz, count => S);
 	L1 : leddec
-	PORT MAP(dig => "00", data => S, anode => anode, seg => seg);
+	PORT MAP(dig => "000", data => S, anode => anode, seg => seg);
 END Behavioral;
