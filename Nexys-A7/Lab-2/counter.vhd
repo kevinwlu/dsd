d972e507d19876abec7666cea74edc89b0776fbe
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-27 20:56:02 -0400

    Update counter.vhd

--- Nexys-A7/Lab-2/counter.vhd
+++ Nexys-A7/Lab-2/counter.vhd
@@ -8,7 +8,7 @@ ENTITY counter IS
 	PORT (
 		clk : IN STD_LOGIC;
 		count : OUT STD_LOGIC_VECTOR (15 DOWNTO 0); -- NEED REVISE! 16 bits
-		mpx : OUT STD_LOGIC_VECTOR (1 DOWNTO 0)); -- NEW ONE ADD! send signal to select displays
+		mpx : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)); -- NEW ONE ADD! send signal to select displays
 END counter;
 
 ARCHITECTURE Behavioral OF counter IS
@@ -21,5 +21,5 @@ BEGIN
 		END IF;
 	END PROCESS;
 	count <= cnt (38 DOWNTO 23); -- 16 bits
-	mpx <= cnt (18 DOWNTO 17); -- 2 bits
+	mpx <= cnt (19 DOWNTO 17); -- 2 bits
 END Behavioral;
