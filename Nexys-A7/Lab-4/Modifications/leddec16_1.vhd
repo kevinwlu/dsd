e2687ac27bde23e9a5315cac92c3b8c8bfaae4d5
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-27 21:41:24 -0400

    Update leddec16_1.vhd

--- Nexys-A7/Lab-4/Modifications/leddec16_1.vhd
+++ Nexys-A7/Lab-4/Modifications/leddec16_1.vhd
@@ -18,22 +18,22 @@ BEGIN
 	         data(11 DOWNTO 8) WHEN dig = "10" ELSE --digit 2
 	         data(15 DOWNTO 12); --digit 3
 	-- Turn on segments corresponding to 4-bit data word
-	seg <= "0000001" WHEN data4 = "0000" ELSE --0
-	       "1001111" WHEN data4 = "0001" ELSE --1
-	       "0010010" WHEN data4 = "0010" ELSE --2
-	       "0000110" WHEN data4 = "0011" ELSE --3
-	       "1001100" WHEN data4 = "0100" ELSE --4
-	       "0100100" WHEN data4 = "0101" ELSE --5
-	       "0100000" WHEN data4 = "0110" ELSE --6
-	       "0001111" WHEN data4 = "0111" ELSE --7
-	       "0000000" WHEN data4 = "1000" ELSE --8
-	       "0000100" WHEN data4 = "1001" ELSE --9
-	       "0001000" WHEN data4 = "1010" ELSE --A
-	       "1100000" WHEN data4 = "1011" ELSE --B
-	       "0110001" WHEN data4 = "1100" ELSE --C
-	       "1000010" WHEN data4 = "1101" ELSE --D
-	       "0110000" WHEN data4 = "1110" ELSE --E
-	       "0111000" WHEN data4 = "1111" ELSE --F
+	seg <= "0000001" WHEN data4 = "0000" ELSE -- 0
+	       "1001111" WHEN data4 = "0001" ELSE -- 1
+	       "0010010" WHEN data4 = "0010" ELSE -- 2
+	       "0000110" WHEN data4 = "0011" ELSE -- 3
+	       "1001100" WHEN data4 = "0100" ELSE -- 4
+	       "0100100" WHEN data4 = "0101" ELSE -- 5
+	       "0100000" WHEN data4 = "0110" ELSE -- 6
+	       "0001111" WHEN data4 = "0111" ELSE -- 7
+	       "0000000" WHEN data4 = "1000" ELSE -- 8
+	       "0000100" WHEN data4 = "1001" ELSE -- 9
+	       "0001000" WHEN data4 = "1010" ELSE -- A
+	       "1100000" WHEN data4 = "1011" ELSE -- B
+	       "0110001" WHEN data4 = "1100" ELSE -- C
+	       "1000010" WHEN data4 = "1101" ELSE -- D
+	       "0110000" WHEN data4 = "1110" ELSE -- E
+	       "0111000" WHEN data4 = "1111" ELSE -- F
 	       "1111111";
 	-- Turn on anode of 7-segment display addressed by 2-bit digit selector dig
 	anode <= "1110" WHEN dig = "00" and data /= X"0000" ELSE -- digit 0
