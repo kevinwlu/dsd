2f484098eddc557e7c91f3dba4ffb7193d561469
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-27 20:59:00 -0400

    Update hexcount.vhd

--- Nexys-A7/Lab-2/hexcount.vhd
+++ Nexys-A7/Lab-2/hexcount.vhd
@@ -56,10 +56,6 @@ BEGIN
 	display <= S(3 DOWNTO 0) WHEN md = "000" ELSE
 	           S(7 DOWNTO 4) WHEN md = "001" ELSE
 	           S(11 DOWNTO 8) WHEN md = "010" ELSE
-	           S(15 DOWNTO 12) WHEN md = "011" ELSE
-	           S(19 DOWNTO 16) WHEN md = "100" ELSE
-	           S(23 DOWNTO 20) WHEN md = "101" ELSE
-	           S(27 DOWNTO 24) WHEN md = "110" ELSE
-	           S(31 DOWNTO 28);
+	           S(15 DOWNTO 12);
 
 END Behavioral;
