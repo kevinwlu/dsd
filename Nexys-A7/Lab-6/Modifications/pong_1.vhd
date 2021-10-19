b12d355ab71b4e937dafacb59566392890985eea
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-10-19 09:56:23 -0400

    Update pong_1.vhd

--- Nexys-A7/Lab-6/Modifications/pong_1.vhd
+++ Nexys-A7/Lab-6/Modifications/pong_1.vhd
@@ -17,7 +17,7 @@ ENTITY pong IS
         ADC_SDATA2 : IN STD_LOGIC;
         btn0 : IN STD_LOGIC; -- button to initiate serve
         SW : IN STD_LOGIC_VECTOR (4 DOWNTO 0); -- ball speed
-        SEG7_anode : OUT STD_LOGIC_VECTOR (3 DOWNTO 0); -- anodes of four 7-seg displays
+        SEG7_anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); -- anodes of four 7-seg displays
         SEG7_seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0) -- common segments of 7-seg displays
         );
 END pong;
